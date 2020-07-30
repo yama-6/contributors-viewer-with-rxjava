efee56779e3cac96ed05fc36121018cde89b834f
yama-6 <ma1006.0204@gmail.com>
2020-07-31 02:14:42 +0900

    不要なコードを削除

--- app/src/main/java/com/android/example/contributorsviewer/MainActivity.kt
+++ app/src/main/java/com/android/example/contributorsviewer/MainActivity.kt
@@ -20,16 +20,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val contributors = GithubApi.getContributors().await()
-
-            Timber.d(contributors.size.toString())
-            contributors.forEach {
-                Timber.d("user name:%s", it.userName)
-                Timber.d("avatar url:%s", it.avatarUrl)
-            }
-        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
