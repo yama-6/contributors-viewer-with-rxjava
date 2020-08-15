8b1782012f189561af1819525d3477c2c4da1914
yuma.yamashita <yuma.yamashita.6@gmail.com>
2020-08-15 15:45:18 +0900

    fix: WebView の Configuration change 対応

--- app/src/main/java/com/android/example/contributorsviewer/userpage/UserPageFragment.kt
+++ app/src/main/java/com/android/example/contributorsviewer/userpage/UserPageFragment.kt
@@ -7,11 +7,19 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import com.android.example.contributorsviewer.databinding.UserPageFragmentBinding
 
 // User page means Github user page of the contributor.
 class UserPageFragment : Fragment() {
     private var webView: WebView? = null
+        set(value) {
+            when (lifecycle.currentState) {
+                Lifecycle.State.CREATED -> field = value
+                else -> throw IllegalStateException()
+            }
+        }
+
     val canWebViewGoBack: Boolean
         get() = webView?.canGoBack() ?: false
 
@@ -31,11 +39,25 @@ class UserPageFragment : Fragment() {
             .fromBundle(requireArguments()).userPageUrl
 
         webView = binding.webview.apply {
-            loadUrl(userPageUrl)
+            when (savedInstanceState) {
+                null -> loadUrl(userPageUrl)
+                else -> {
+                    restoreState(savedInstanceState)
+                    when (url) {
+                        null -> loadUrl(userPageUrl)
+                    }
+                }
+            }
+
             webViewClient = WebViewClient()
         }
 
         return binding.root
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        webView!!.saveState(outState)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onDestroyView() {
